5a67f2411517a55cd7cf50c1ef57f9894197f41f
JulioCaballero <173180@ids.upchiapas.edu.mx>
2020-06-14 21:12:11 -0500

    added imports

--- proyecto_flutter/lib/src/app.dart
+++ proyecto_flutter/lib/src/app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+//Importaciones de paginas
 import 'package:proyecto_flutter/src/pages/flutter_page.dart';
 import 'package:proyecto_flutter/src/pages/react_page.dart';
-
-//Importaciones de paginas
 import 'package:proyecto_flutter/src/pages/sign_in.dart';
 import 'package:proyecto_flutter/src/pages/details_page.dart';
 import 'package:proyecto_flutter/src/pages/hello_flutter.dart';
